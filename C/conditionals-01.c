44e317b9e25607080e68d05ef4d3d471b8ed10fb
Chris <atnl@protonmail.com>
2024-10-18 19:22:32 -0400

    Create conditionals-01.c

---
+++ C/conditionals-01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Example of a basic conditional statement: if-else
+int main() {
+    int x = 10;
+    if (x > 5) {
+        printf("x is greater than 5\n");
+    } else {
+        printf("x is not greater than 5\n");
+    }
+
+    return 0;
+}
+
+// Another example
+//int main() {
+//    int x = 4;
+//    if (x < 0) printf("x is negative\n");
+//    else { // or you can use a block
+//        printf("x is positive\n");
+//    }
+//    return 0;
+//}
