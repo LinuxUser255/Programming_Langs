7c853d5f33f834409774b619b4bc2b33d606c371
Chris <atnl@protonmail.com>
2024-10-18 20:41:24 -0400

    Create variables.rs

---
+++ Rust/variables.rs
@@ -0,0 +1,16 @@
+// Basic example of use of variables in Rust
+fn main() {
+    // Declare and initialize a variable of type integer
+    let x = 5;
+
+    // Declare and initialize a variable of type string
+    let y = "Hello, world!";
+
+    // Declare and initialize a variable of type boolean
+    let z = true;
+
+    // Print the values of the variables
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
+}
