5fd0d792d343c1dada9fcb31ab102116915fe457
Chris <atnl@protonmail.com>
2024-10-18 20:14:51 -0400

    Create forloop.go

---
+++ Go/forloop.go
@@ -0,0 +1,14 @@
+// Basice example of a for loop in Go
+package main
+
+import "fmt"
+
+func main() {
+    // Initialize a variable
+    var i int = 0
+
+    // Iterate over a range of numbers
+    for i = 0; i < 5; i++ {
+        fmt.Println(i)
+    }
+}
